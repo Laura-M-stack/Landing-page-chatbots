1271aaa92154b7e91696bfa1a93bfe87016332c8
Laura Moyano <lauraymh000@hotmail.com>
2025-01-03 22:51:54 -0300

    Agrego modo oscuro

---
+++ script.js
@@ -0,0 +1,31 @@
+// Selección de elementos
+const themeToggle = document.querySelector('.theme-toggle');
+const sunIcon = document.getElementById('sun-icon');
+const moonIcon = document.getElementById('moon-icon');
+const root = document.documentElement;
+
+// Verifica el modo inicial
+if (localStorage.getItem('theme') === 'dark') {
+    root.classList.add('dark-mode');
+    sunIcon.style.display = 'inline-block';
+    moonIcon.style.display = 'none';
+}
+
+// Alterna el modo y actualiza íconos
+themeToggle.addEventListener('click', () => {
+    if (root.classList.contains('dark-mode')) {
+        root.classList.remove('dark-mode');
+        localStorage.setItem('theme', 'light');
+        sunIcon.style.display = 'none';
+        moonIcon.style.display = 'inline-block';
+    } else {
+        root.classList.add('dark-mode');
+        localStorage.setItem('theme', 'dark');
+        sunIcon.style.display = 'inline-block';
+        moonIcon.style.display = 'none';
+    }
+});
+
+
+
+
